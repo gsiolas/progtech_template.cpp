7c0067ac48c6f5e3c09b188b226d6c06196fe36f
Giorgos Siolas <gsiolas@islab.ntua.gr>
2020-03-07 09:37:08 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// cpp template with debug symbols compilation. git ignores main executable
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+string greeting = "Hello Progtech 2020!";
+  
+cout << greeting << endl;
+}
